0daab8bb26a4bde05d35acf131902b15aa8d0551
Puskor Roy <puskarkumarroy329@gmail.com>
2025-03-25 11:44:53 +0600

    postsql add sucsses

--- task_management/settings.py
+++ task_management/settings.py
@@ -75,12 +75,25 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # or your database host
+        'PORT': '5432',  # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
