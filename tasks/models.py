17de8cbddffa3c0273ff2bd7b624cfad3dbf51ba
Puskor Roy <puskarkumarroy329@gmail.com>
2025-03-25 22:02:20 +0600

    work complte models

--- tasks/models.py
+++ tasks/models.py
@@ -5,9 +5,14 @@ from django.db import models
 class Project(models.Model):
     name=models.CharField(max_length=50)
     start_date=models.DateField()
+    
+class Employee(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True)
 
 class Task(models.Model):
     project=models.ForeignKey(Project,on_delete=models.CASCADE,default=1)
+    employee=models.ManyToManyField(Employee)
     title=models.CharField(max_length=150)
     description=models.TextField()
     due_date=models.DateField()
